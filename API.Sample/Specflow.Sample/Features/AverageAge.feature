d9cc4270e486f61675433863f235612a8cdc2a5b
Ian Sharp <Ian.Sharp@dominos.co.uk>
2023-08-09 10:23:19 +0100

    Initial commit

---
+++ API.Sample/Specflow.Sample/Features/AverageAge.feature
@@ -0,0 +1,8 @@
+﻿Feature: AverageAge
+
+A short summary of the feature
+
+Scenario: Average Age Test
+	##Given I have a persons name
+	When I request the average age of a persons name
+	##Then the expected details are returned
---
+++ API.Sample/Specflow.Sample/StepDefinitions/AverageAgeSteps.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using RestSharp;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace SpecFlowProject.StepDefinitions
+{
+    public class AverageAge
+    {
+        public int Count { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    [Binding]
+    public sealed class AverageAgeSteps
+    {
+        [When(@"I request the average age of a persons name")]
+        public async Task WhenIRequestTheAverageOfAPersonsName()
+        {
+            //Given
+            var client = new RestClient();
+            var request = new RestRequest("https://api.agify.io?name=Dave", Method.Get);
+
+            //When
+            var response = await client.GetAsync(request);
+
+            //Then
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            var responseJson = JsonSerializer.Deserialize<AverageAge>(response.Content ?? "", options);
+
+            responseJson?.Name.Should().Be("Dave");
+            responseJson?.Age.Should().Be(67);
+        }
+    }
+}
